1bb5618d750b91ec62fd806749ed11b2608eb9aa
Guacamoles123 <Migueldominguez@sos-sistemas.com>
2023-11-09 16:00:47 +0000

    Cambio fichero
    Añadido un comentario en el fichero...

--- Assets/Animados/Trigger/DesaparecerLlave.cs
+++ Assets/Animados/Trigger/DesaparecerLlave.cs
@@ -13,7 +13,7 @@ public class DesaparecerLlave : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
